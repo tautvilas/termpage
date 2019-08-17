27bf46dba40c0595f00040416ad7261be065c281
tautvilas <mecinskas@gmail.com>
2019-08-17 21:25:59 +0300

    feat: append input to output

--- textual.js
+++ textual.js
@@ -4,3 +4,25 @@ document.getElementsByTagName("body")[0].addEventListener("click", function(){
   document.getElementById('input').focus();
 });
 
+const Textual = {
+  appendOutput: (output) => {
+    const prmpt = "$ ";
+    var pre = document.createElement("pre");;
+    var textNode = document.createTextNode(prmpt + output + '\n');
+    pre.appendChild(textNode);
+    document.getElementById('output').appendChild(pre);
+  }
+};
+
+function processInput(input) {
+  Textual.appendOutput(input);
+}
+
+document.getElementById('input').addEventListener('keypress', function (e) {
+  var key = e.which || e.keyCode;
+  if (key === 13) { // 13 is enter
+    processInput(e.srcElement.value);
+    e.srcElement.value = '';
+    document.getElementById('window').scrollTo(0, document.getElementById("window").scrollHeight);
+  }
+});
