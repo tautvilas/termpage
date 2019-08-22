3b143f0b5bb388a28fcd933eb1edef1159b5726a
tautvilas <mecinskas@gmail.com>
2019-08-22 23:00:04 +0300

    improve example

--- example/main.js
+++ example/main.js
@@ -2,20 +2,21 @@ const homeText = Termpage.replace(`
 ...My name is Tautvilas, welcome to my termpage!...
 ...Type HELP for the list of available commands....\n\n`,
 {
-  commands: Termpage.color('orange'),
+  HELP: Termpage.color('orange'),
   Tautvilas: Termpage.link('http://www.tautvilas.lt'),
 });
 
 function processInput(input='') {
   input = input.toLowerCase().trim();
+  const commands = ['home', 'help', 'image'];
   if (input === "help") {
-    return "Available commands are `home`, `help` and `image`";
+    return {text: "Available commands are `home`, `help` and `image`", commands: commands};
   } else if (input === 'home') {
-    return homeText;
+    return {text: homeText, commands: commands};
   } else if (input === 'image') {
-    return '<img width="200" height="200" src="https://i.imgur.com/RDsb26sb.jpg" alt="me"/>';
+    return {text: '<img width="200" height="200" src="https://i.imgur.com/RDsb26sb.jpg" alt="me"/>', commands: commands};
   } else {
-    return 'Command not found\n';
+    return {text: 'Command not found\n', commands: commands};
   }
 }
 
@@ -30,13 +31,15 @@ Termpage.init(
 Termpage.init(
   document.getElementById('window2'),
   (input) => {
+    if (input === 'home') {
+      return 'This terminal demonstrates async commands that have 50% chance of failing';
+    }
     let resolveP;
     const promise = new Promise((resolve, reject) => {
       setTimeout(() => {
         if (Math.random() > 0.5) {
           resolve({
-            text: input + " " + input,
-            commands: ['Home', 'About', Termpage.color('yellow', 'Help')]
+            text: 'async request was successfull'
           });
         } else {
           reject()
@@ -46,8 +49,8 @@ Termpage.init(
     return promise;
   },
   {
-    initialCommand: 'Async promise example',
-    prompt: Termpage.color('green', 'tautvilas@termpge') + ':',
+    initialCommand: 'home',
+    prompt: Termpage.color('green', 'type_anything:') + ':',
     autoFocus: false
   }
 );
